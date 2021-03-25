3838cea53eaa618c966c521490b1749d3d7134f8
Ching Ling Yeung <chinglingyeung1@gmail.com>
2021-03-25 16:00:39 +0000

    API to Pelion

--- api.py
+++ api.py
@@ -1,5 +1,26 @@
 import numpy as np
 import time
+from mbed_cloud.foundation import Device
+from mbed_cloud import ConnectAPI
+from mbed_cloud.subscribe import *
+from mbed_cloud.subscribe.channels import ResourceValues
+# from mbed_cloud import ResourceValues
+
+api = ConnectAPI()
+
+# for device in Device().list(max_results=10):
+#     print("Hello device %s" % device.name)
+
+# notification = api.subscribe(
+#   api.subscribe.channels.DeviceStateChanges(device_id=1234)
+# ).next().block()
+# print(notification)
+# channel = subscribe.channels(ResourceValues(resource_path=['/4/0/1', '/3/*']))
+# api.subscribe.channels.ResourceValues()
+# test = ResourceValues(resource_path=['/4/0/1', '/3/*'])
+channel = api.subscribe(ResourceValues(resource_path=['/3313/*', '/3334/*']))
+for result in channel:
+  print(result.block())
 
 def data_generator():
     interval = 0.2
